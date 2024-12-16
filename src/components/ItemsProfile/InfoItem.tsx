2ebb474f9c106fd3c27632840fcb22f147b04aad
unknown <UstinovichSergey>
2024-12-16 13:32:14 +0300

    InventoryAndLink?

---
+++ src/components/ItemsProfile/InfoItem.tsx
@@ -0,0 +1,42 @@
+import toast from "react-hot-toast";
+import { InventoryType } from "../../types/InventoryType";
+import { Button } from "../../ui/Button";
+import style from "./ItemsProfile.module.scss";
+
+function InfoItem({ item }: { item: InventoryType }) {
+    
+
+  const copyToRefLink = async () => {
+    try {
+      if (item.promo_code) {
+        await navigator.clipboard.writeText(item.promo_code);
+        toast.success('Ссылка скопирована в буфер обмена')
+      }
+    } catch (err) {
+        toast.error('Ошибка при копировании')
+    }
+  };
+
+  return (
+    <div className={style.boxInfo}>
+      {/* <img
+          className={style.imgWinner}
+          src={`https://api.zerkalogm.online${item.image_without_background_url}`}
+          alt=""
+        /> */}
+      <img
+        className={style.imgWinner}
+        src={`https://api.zerkalogm.online/${item.image}`}
+        alt=""
+      />
+      <div onClick={copyToRefLink} className={style.boxCopy}>
+        <h3 className={style.title}>Click to copy the code</h3>
+      </div>
+      <a target="_blank" className={style.link} href={item.url_product}>
+       <Button className={style.btn}>Activate</Button>
+      </a>
+    </div>
+  );
+}
+
+export default InfoItem;
--- src/components/ItemsProfile/ItemsProfile.tsx
+++ src/components/ItemsProfile/ItemsProfile.tsx
@@ -6,15 +6,17 @@ import { inventoryUser } from "../../api/userInfo";
 import { useTelegram } from "../../providers/telegram/telegram";
 import { useEffect, useState } from "react";
 import { InventoryType } from "../../types/InventoryType";
+import Modal from "../../ui/Modal/Modal";
+import InfoItem from "./InfoItem";
 
 const ITEMS_COUNT = 12;
 const MIN_EMPTY_SLOTS = 3;
 
-
-
 function ItemsProfile() {
   const { tg_id } = useTelegram();
-  const [data, setData] = useState()
+  const [data, setData] = useState();
+  const [isOpen, setIsOpen] = useState(false);
+  const [infoItems, setInfoItems] = useState<InventoryType>();
 
   const inventoryQuery = useQuery(
     {
@@ -25,24 +27,47 @@ function ItemsProfile() {
   );
 
   useEffect(() => {
-    if(inventoryQuery.data) {
-        setData(inventoryQuery.data)
+    if (inventoryQuery.data) {
+      setData(inventoryQuery.data);
     }
-  }, [inventoryQuery.data])
-    const inventoryItems: InventoryType[] = data ? createEmptySlots(data, ITEMS_COUNT, MIN_EMPTY_SLOTS) : [];
+  }, [inventoryQuery.data]);
+  const inventoryItems: InventoryType[] = data
+    ? createEmptySlots(data, ITEMS_COUNT, MIN_EMPTY_SLOTS)
+    : [];
+
+  const handleOpen = (id: string | number) => {
+    setIsOpen(true);
+    const items = inventoryItems.find((item) => item.id === id);
+    setInfoItems(items);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+    setInfoItems(undefined);
+  };
 
   return (
-    <div className={style.box}>
-      <h2 className={style.title}>Items</h2>
-      <ul>
-        {inventoryItems.map((item) => (
-            <li key={item.id}>
-                
-                <img src={`https://api.zerkalogm.online${item.image}`} alt="" />
+    <>
+      <div className={style.box}>
+        <h2 className={style.title}>Items</h2>
+        <ul className={style.list}>
+          {inventoryItems.map((item) => (
+            <li
+              onClick={!item.filled ? () => handleOpen(item.id) : () => {}}
+              className={style.item}
+              key={item.id}
+            >
+              <img src={`https://api.zerkalogm.online/${item.image}`} alt="" />
+              {item.filled && <div className={style.emptySlots} />}
+              {!item.filled && <p className={style.descr}>{item.text}</p>}
             </li>
-        ))}
-      </ul>
-    </div>
+          ))}
+        </ul>
+      </div>
+      <Modal isOpen={isOpen} isSpecial closeBtn onClose={handleClose}>
+        {infoItems && <InfoItem item={infoItems} />}
+      </Modal>
+    </>
   );
 }
 
